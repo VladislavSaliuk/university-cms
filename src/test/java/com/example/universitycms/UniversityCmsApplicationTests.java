5dcb3e29f4c3e9559e1cfcff7d089658738b89f0
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-05-14 12:11:05 +0300

    Initial commit

---
+++ src/test/java/com/example/universitycms/UniversityCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitycms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
