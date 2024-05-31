b10ebfc23210e3d893a0dd367346d6069857648e
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-05-31 15:19:04 +0300

    Adding styles for login page!

--- src/main/java/com/example/universitycms/config/SecurityConfiguration.java
+++ src/main/java/com/example/universitycms/config/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
