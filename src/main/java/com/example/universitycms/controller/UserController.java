aa7e280eefa202f87042f019f6c69882ba9da7bc
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-06-09 14:37:36 +0300

    Implementing search filter

--- src/main/java/com/example/universitycms/controller/UserController.java
+++ src/main/java/com/example/universitycms/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.universitycms.controller;
 
+import com.example.universitycms.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/admin-page")
-    public String showAdminPage() {
+    public String showAdminPage(Model model) {
+        model.addAttribute("userList", userService.getAll());
         return "admin-page";
     }
 
