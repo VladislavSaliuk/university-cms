a26101088f5dc766ad08f62788ae68e32439ad63
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-06-15 20:15:21 +0300

    Implement deleting course feature

--- src/main/java/com/example/universitycms/controller/AdminController.java
+++ src/main/java/com/example/universitycms/controller/AdminController.java
@@ -1,17 +1,12 @@
 package com.example.universitycms.controller;
 
-import javax.validation.Valid;
-
 import com.example.universitycms.model.Course;
 import com.example.universitycms.service.CourseService;
 import com.example.universitycms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdminController {
@@ -34,15 +29,21 @@ public class AdminController {
         return "admin-course-page";
     }
 
-    @GetMapping("/admin/courses/add-course")
-    public String showAddCoursePage(@ModelAttribute Course course, Model model) {
+    @GetMapping("/admin/courses/course-form")
+    public String showCourseFormPage(@ModelAttribute Course course, Model model) {
         model.addAttribute("course", course);
-        return "add-course-page";
+        return "course-form-page";
     }
 
     @PostMapping("/admin/courses/add-course")
-    public String createCourse( @ModelAttribute Course course) {
+    public String createCourse(@ModelAttribute Course course) {
         courseService.createCourse(course);
+        return "redirect:/admin/courses";
+    }
+
+    @GetMapping("/admin/courses/delete-course/{courseId}")
+    public String deleteCourse(@PathVariable long courseId) {
+        courseService.removeCourseByCourseId(courseId);
         return "redirect:/admin/courses";
     }
 
--- src/main/java/com/example/universitycms/repository/CourseRepository.java
+++ src/main/java/com/example/universitycms/repository/CourseRepository.java
@@ -3,11 +3,14 @@ package com.example.universitycms.repository;
 import com.example.universitycms.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     void deleteAll();
+
+    @Transactional
     void deleteCourseByCourseId(long courseId);
     void deleteCourseByCourseName(String courseName);
     Course findCourseByCourseName(String courseName);
