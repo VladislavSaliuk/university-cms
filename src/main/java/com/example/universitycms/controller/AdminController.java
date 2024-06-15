cd375796d4a466903414e9b7578608cc5a5d6c38
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-06-15 17:19:19 +0300

    Implement adding course feature

--- src/main/java/com/example/universitycms/controller/AdminController.java
+++ src/main/java/com/example/universitycms/controller/AdminController.java
@@ -1,11 +1,17 @@
 package com.example.universitycms.controller;
 
+import javax.validation.Valid;
+
+import com.example.universitycms.model.Course;
 import com.example.universitycms.service.CourseService;
 import com.example.universitycms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
@@ -28,5 +34,16 @@ public class AdminController {
         return "admin-course-page";
     }
 
+    @GetMapping("/admin/courses/add-course")
+    public String showAddCoursePage(@ModelAttribute Course course, Model model) {
+        model.addAttribute("course", course);
+        return "add-course-page";
+    }
+
+    @PostMapping("/admin/courses/add-course")
+    public String createCourse( @ModelAttribute Course course) {
+        courseService.createCourse(course);
+        return "redirect:/admin/courses";
+    }
 
 }
