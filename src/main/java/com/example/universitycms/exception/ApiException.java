6ecf1669552551f3f12d60befa42130a5dbd53ba
Vladislav Saliuk <seattlemusic345@gmail.com>
2024-07-23 12:40:40 +0300

    Mistake correction 14!

--- src/main/java/com/example/universitycms/exception/ApiException.java
+++ src/main/java/com/example/universitycms/exception/ApiException.java
@@ -6,26 +6,19 @@ import java.time.ZonedDateTime;
 
 public class ApiException {
     private final String message;
-    private final Throwable throwable;
     private final HttpStatus httpStatus;
     private final ZonedDateTime timeStamp;
 
     public ApiException(String message,
-                        Throwable throwable,
                         HttpStatus httpStatus,
                         ZonedDateTime timeStamp) {
         this.message = message;
-        this.throwable = throwable;
         this.httpStatus = httpStatus;
         this.timeStamp = timeStamp;
     }
 
     public String getMessage() {
         return message;
-    }
-
-    public Throwable getThrowable() {
-        return throwable;
     }
 
     public HttpStatus getHttpStatus() {
--- src/main/java/com/example/universitycms/exception/ApiRequestExceptionHandler.java
+++ src/main/java/com/example/universitycms/exception/ApiRequestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.universitycms.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,15 +12,16 @@ import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApiRequestExceptionHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(ApiRequestExceptionHandler.class);
     @ExceptionHandler(value = {ApiRequestException.class})
     public ResponseEntity<Object> handleApiRequestExceptionHandler(ApiRequestException e) {
 
+        logger.error("API Request Exception: ", e);
+
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
-        ApiException apiException = new ApiException (
+        ApiException apiException = new ApiException(
                 e.getMessage(),
-                e,
                 badRequest,
                 ZonedDateTime.now(ZoneId.of("Z"))
         );
